2078de367c379e875a0700c8a1bc0e32e78e4f9c
BbaGoun <80041912+BbaGoun@users.noreply.github.com>
2026-07-09 16:10:58 +0900

    7/9 Obj PR 전에 잠깐 수정
    Get/Set 함수 추가

--- Direct3D_Team_Assignment/Direct3D_Team_Assignment/CObj.h
+++ Direct3D_Team_Assignment/Direct3D_Team_Assignment/CObj.h
@@ -19,11 +19,22 @@ public:
 	const D3DXVECTOR3& GetDir() { return m_tINFO.vDir; }
 	const D3DXVECTOR3& GetLook() { return m_tINFO.vLook; }
 	const D3DXVECTOR3& GetNormal() { return m_tINFO.vNormal; }
+	const float& GetSpeed() { return m_fSpeed; }
+	const float& GetRadian() { return m_fRadian; }
+	const int& GetHP() { return m_iHP; }
+	const int& GetMaxHP() { return m_iMaxHP; }
+	const int& GetDamage() { return m_iDamage; }
 
 	void SetPos(D3DXVECTOR3 _vPos) { m_tINFO.vPos = _vPos; }
 	void SetDir(D3DXVECTOR3 _vDir) { m_tINFO.vDir = _vDir; }
 	void SetLook(D3DXVECTOR3 _vLook) { m_tINFO.vLook = _vLook; }
 	void SetNormal(D3DXVECTOR3 _vNormal) { m_tINFO.vNormal = _vNormal; }
+	void SetSpeed(float _fSpeed) { m_fSpeed = _fSpeed; }
+	void SetRadian(float _fRadian) { m_fRadian = _fRadian; }
+	void SetHP(int _iHP) { m_iHP = _iHP; }
+	void SetMaxHP(int _iMaxHP) { m_iMaxHP = _iMaxHP; }
+	void SetDamage(int _iDamage) { m_iDamage = _iDamage; }
+	
 
 protected:
 
@@ -32,9 +43,9 @@ protected:
 	float m_fSpeed;
 	float m_fRadian;
 
-	int iHP;
-	int iMaxHP;
+	int m_iHP;
+	int m_iMaxHP;
 
-	int iDamage;
+	int m_iDamage;
 };
 
